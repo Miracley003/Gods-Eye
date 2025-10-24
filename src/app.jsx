a01c9ac79665177b9ff44d0b9686254198379b87
Miracle King <161554622+Miracley003@users.noreply.github.com>
2025-10-24 11:50:20 +0100

    Create initial App component for Gods Eye DApp

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Gods Eye DApp</h1>
+      {/* Your app content */}
+    </div>
+  )
+}
+
+export default App
